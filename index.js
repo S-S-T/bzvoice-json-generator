557e6647c459f2f1a6b9c0c1efeb5a83719749cc
Schaffer <schaffer.st@pg.com>
2019-01-05 01:50:06 -0500

    prettify the results

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const axios = require('axios');
 const fs = require('fs');
 const bzUrl = 'stg.api.bazaarvoice.com';
-const version = '5.4';
+const version = 'apiversion=5.4';
 const passkey = 'cafRvoiYDDRNKRPiOwwRfb4snRoHfdFbd9fpsX1JlRJ5M';
 
-const reviewsUrl = `https://${bzUrl}/data/reviews.json?apiversion=${version}?passkey=${passkey}`;
+const reviewsUrl = `https://${bzUrl}/data/reviews.json?${version}&passkey=${passkey}`;
 const reviews = [];
-var reviewJson = {};
-
+let i = 0
 
 const getBZreviews = async () => {
   const response = await axios.get(reviewsUrl);
-  await response.data.reviews.map(review => {
-    reviews.push(review.id);
-      const json = await JSON.stringify({
-        bz_reviews: reviewJson
-      });
-      await fs.writeFile('bzreviews.json', json, 'utf8', err => {
-        console.log(err);
-      });
-    }
-  )};
+  await response.data.Results.map(result => {
+    reviews.push(response.data.Results[i += 1]);
+    result;
+  })
+  
+  const json = await JSON.stringify({
+	bz_reviews: reviews
+  }, null, 4);
+  
+  await fs.writeFile('bzreviews.json', json, 'utf8', err => {
+	console.log(err);
+  });
+};
 
 getBZreviews();
